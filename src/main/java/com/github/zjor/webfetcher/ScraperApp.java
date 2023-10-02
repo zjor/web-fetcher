678b9f30e47d845ca78237350808dc656838bb68
Sergey Royz <zjor.se@gmail.com>
2023-10-02 23:00:45 +0200

    swagger UI added

--- src/main/java/com/github/zjor/webfetcher/ScraperApp.java
+++ src/main/java/com/github/zjor/webfetcher/ScraperApp.java
@@ -3,9 +3,7 @@ package com.github.zjor.webfetcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ScraperApp {
